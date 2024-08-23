8e73fa940db95b8207eef80c2d61e7e503b904ec
CHENJIAR3 <87299283+CHENJIAR3@users.noreply.github.com>
2024-08-23 18:30:53 +0800

    Update compare_models_copy.py

--- training_code/compare_models_copy.py
+++ training_code/compare_models_copy.py
@@ -1,33 +1,26 @@
-# 导入库函数
-# 这训练的函数为copy
+
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '6,7'
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from  model_new import modelx
-gpus = tf.config.experimental.list_physical_devices('GPU')
-assert len(gpus) > 0, "Not enough GPU hardware devices available"
-for i in range(len(gpus)):
-    tf.config.experimental.set_memory_growth(gpus[i], True)
-#首先引入策略
-strategy = tf.distribute.MirroredStrategy()
 
 from utils import args,read_tfrecords
-from testmodel.testecg_model import *
+from testmodel.testecg_model import test_ae,write2excel
 def extract(a, t, x_shape):
     batch_size, sequence_length, _ = a.shape
     t_shape = tf.shape(t)
     out = tf.gather(a, t, axis=-1)
     out = tf.reshape(out, (batch_size, t_shape[0], *((1,) * (len(x_shape) - 1))))
     return out
+    
 def getcc(ecg12,gen_ecg12):
     return (tf.reduce_mean(ecg12 * gen_ecg12, axis=[1]) - tf.reduce_mean(ecg12, axis=[1]) * tf.reduce_mean(
         gen_ecg12, axis=[1])) \
          / (tf.math.reduce_std(ecg12, axis=[1]) * tf.math.reduce_std(gen_ecg12, axis=[1]))
 
 
-class Trainer:
+class Trainer_copy:
     def __init__(self,modelpath,epochs=100, lr=1e-3, ecglen=1024,kernel_size=5,pool_size = 2
                  , step_total = 340, anylead = 1, patience = 50):
 
@@ -186,29 +179,16 @@ class Trainer:
         return self.val_loss_tracker.result()
 
 if __name__ == "__main__":
-    if args.data_norm:
-        trainpath = "/data/0shared/chenjiarong/lead_dataset/trainset_lead"
-        testpath = "/data/0shared/chenjiarong/lead_dataset/testset_lead"
-        valpath = "/data/0shared/chenjiarong/lead_dataset/valset_lead"
-    else:
-        trainpath = "/data/0shared/chenjiarong/lead_dataset/trainset2_lead"
-        testpath = "/data/0shared/chenjiarong/lead_dataset/testset2_lead"
-        valpath = "/data/0shared/chenjiarong/lead_dataset/valset2_lead"
-    testpath2 = '/data/0shared/chenjiarong/lead_dataset/cpsc2018'
-    testds2 = read_tfrecords(testpath2).batch(args.bs).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
-    resultpath = '../results/ptbxl/'
+    # putting your trainpath\valpath\testpath
     trainds = read_tfrecords(trainpath).prefetch(
         buffer_size=tf.data.experimental.AUTOTUNE).batch(args.bs).shuffle(1000)
     valds = read_tfrecords(valpath).batch(args.bs).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
     testds = read_tfrecords(testpath).batch(args.bs).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
-    testpath2 = '/data/0shared/chenjiarong/lead_dataset/cpsc2018'
-    testds2 = read_tfrecords(testpath2).batch(args.bs).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    # result
     resultpath = '../results/ptbxl/'
-    resultpath2 = '../results/cpsc2018/'
 
     if not os.path.exists(resultpath):
         os.mkdir(resultpath)
-        os.mkdir(resultpath2)
     # valds=strategy.experimental_distribute_dataset(valds)
     # trainds=strategy.experimental_distribute_dataset(trainds)
     args.step_total=87200//args.bs    #
@@ -216,13 +196,10 @@ if __name__ == "__main__":
         modelpath = "Autoencoder_ks5_copy_"+str(anylead)
         args.anylead=anylead
         args.padding=padding
-        trainer_ae = Trainer(modelpath=modelpath,epochs=args.epochs, lr=args.lr, ecglen=args.ecglen)
-        trainer_ae.train(trainds, valds)
+        trainer_copy = Trainer_copy(modelpath=modelpath,epochs=args.epochs, lr=args.lr, ecglen=args.ecglen)
+        trainer_copy.train(trainds, valds)
 
         model = tf.keras.models.load_model(modelpath)
         MAE_test, MSE_test, CC_test = test_ae(model=model, ds=testds, output_num=1)
         excel_path = resultpath +  "Autoencoder_ks5_copy_"+str(anylead)+ '.xlsx'
-        write2excel(MAE_test, MSE_test, CC_test, excel_path)
-        MAE_test, MSE_test, CC_test = test_ae(model=model, ds=testds2, output_num=1)
-        excel_path = resultpath2 + "Autoencoder_ks5_copy_"+str(anylead)+ '.xlsx'
         write2excel(MAE_test, MSE_test, CC_test, excel_path)
